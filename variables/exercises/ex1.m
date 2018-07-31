cb9d2514bbfe77968f83cd5e2012438fe6361b3f
Aritra Sur Roy <aritra1997@hotmail.com>
2018-07-31 17:47:16 +0530

    Exercises of chapter

---
+++ variables/exercises/ex1.m
@@ -0,0 +1,4 @@
+% exercise 1 (1)
+name = 'Aritra'
+name1 = "Aritra" 
+ 
---
+++ variables/exercises/ex3.m
@@ -0,0 +1,5 @@
+% exercise 3
+inflation_rate = 0.03;
+% exercise 3 a
+money = 1000000;
+money_after_infaltion = money * (1 + inflation_rate)^-(65 - 25)
